8c3f9bdd4dae0b4b085be9897cf9655b61c80e6f
portasynthinca3 <portasynthinca3@gmail.com>
2021-03-24 20:35:46 +0100

    prevent usernames in VC from jumping around

--- src/tasty/tasty.erl
+++ src/tasty/tasty.erl
@@ -37,7 +37,8 @@ init(_Args) ->
 
 broadcast_connected(Channel) ->
     States = [{User, UserState} || {_, User, UserState, _} <- ets:lookup(channel_users, Channel)],
-    {Users, Statuses} = lists:unzip(States),
+    Sorted = lists:sort(fun({A, _}, {B, _}) -> A =< B end, States),
+    {Users, Statuses} = lists:unzip(Sorted),
     normal_client:icpc_broadcast_to_aware(chan_awareness,
         #entity{type=channel, fields=#{
             id => Channel,
