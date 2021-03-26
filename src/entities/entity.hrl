952c2a32cbcfa26d01ce635614a552f4348286a9
portasynthinca3 <portasynthinca3@gmail.com>
2021-03-26 09:45:48 +0100

    add Exhibit A to each source file

--- src/entities/entity.hrl
+++ src/entities/entity.hrl
@@ -1,3 +1,7 @@
+%%% This Source Code Form is subject to the terms of the Mozilla Public
+%%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -record(entity, {type=unknown, fields=#{}}).
 -record(entity_pagination, {field=0, dir=unknown, from=0, cnt=0}).
 -record(entity_context, {type=unknown, id=0}).
--- src/packets/packet.hrl
+++ src/packets/packet.hrl
@@ -1,3 +1,7 @@
+%%% This Source Code Form is subject to the terms of the Mozilla Public
+%%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -record(packet, {type=unknown, seq=0, reply=0, captcha="", fields=#{}}).
 
 -define(REVERSE_PACKET_TYPE_MAP, utils:swap_map(?PACKET_TYPE_MAP)).
