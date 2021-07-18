4f9f29a47623f16295f127a6db73b96ec000a1a8
portasynthinca3 <portasynthinca3@gmail.com>
2021-07-18 14:57:47 +0200

    more robust tasty processing

--- src/tasty/tasty_client.erl
+++ src/tasty/tasty_client.erl
@@ -8,12 +8,12 @@
 -description("The Tasty (voice/video protocol) UDP listener").
 
 -define(TIMEOUT, 15000).
--define(PACKET_RATE_LIMIT, 100).
+-define(PACKET_RATE_LIMIT, 150).
 -define(PACKET_SIZE_LIMIT, 128).
 -define(SPEAKING_IND_THRESHOLD, 250).
 
 -export([handle_packet/2, handler/2, controller_init/2]).
--export([speaking_status_timeout/1]).
+-export([speaking_status_timeout/1, session_timeout/1]).
 
 %%% encryption helpers
 %% encrypt data
@@ -26,37 +26,42 @@ dec_chunk(<<IV:128/bitstring, Data/binary>>, <<Key:128/bitstring>>) ->
 
 handle_packet(Src, Packet) -> spawn(?MODULE, handler, [Src, Packet]).
 
-%% "identitification" packet
+%% identitification packet
 handler(Src={_,_}, <<0, SId:128/bitstring>>) ->
     Session = tasty:get_session(SId),
     tasty:register_user(SId, Src, spawn(?MODULE, controller_init, [Session, Src]));
 
 %% normal packet
 handler(Src={_,_}, <<1, Encrypted/binary>>) ->
-    {_, Key, _, _, Controller} = tasty:get_session(Src),
-    Controller ! {packet, dec_chunk(Encrypted, Key)}.
+        {_, Key, _, _, Controller} = tasty:get_session(Src),
+    Controller ! {packet, dec_chunk(Encrypted, Key)};
+
+handler(_, _) -> ok.
 
 %% voice data
 dec_handler({_, _, User, Chan, _}, <<0, Data/binary>>) ->
     Allow = check_data_lims(Data),
-    if Allow ->
-            tasty:broadcast(Chan, Data, User);
-       true -> drop
+    if
+        Allow -> tasty:broadcast(Chan, Data, User);
+        true -> drop
     end;
 
 %% disconnect notice
 dec_handler(_, <<1>>) -> drop;
 
 %% heartbeat
-dec_handler(_, <<2>>) -> ok.
+dec_handler(_, <<2>>) -> ok;
+
+dec_handler(_, _) ->
+    logging:warn("Invalid packet head (possble decryption failure?)", []),
+    drop.
 
 %% checks voice data packet limits
 check_data_lims(Data) ->
-    true.
-    % (byte_size(Data) =< ?PACKET_SIZE_LIMIT) and
-    % (ratelimit:hit(packet, 1) == 1).
+    (byte_size(Data) =< ?PACKET_SIZE_LIMIT) and
+    (ratelimit:hit(packet) =:= 1).
 
-%%% the controller is responsible for receiving, decypting and parsing
+%%% the controller is responsible for receiving, decrypting and parsing
 %%% data packets from one specific client, asking the local Tasty gen_server
 %%% to broadcast them if needed and accepting and fulfilling broadcast
 %%% request from the aforementioned server
@@ -65,14 +70,16 @@ check_data_lims(Data) ->
 controller_init(Session={Id, Key, _, _, _}, Src) ->
     ratelimit:make(packet, {?PACKET_RATE_LIMIT, 1000}),
     tasty_listener ! {send, Src, enc_chunk(<<0>>, Key)},
-    Timeout = spawn_link(?MODULE, speaking_status_timeout, [Id]),
-    controller_loop(Session, Src, Timeout).
+    SpeakTimeout = spawn_link(?MODULE, speaking_status_timeout, [Id]),
+    Timeout = spawn_link(?MODULE, session_timeout, [self()]),
+    controller_loop(Session, Src, {SpeakTimeout, Timeout}).
 
 %% controller loop
-controller_loop(Session={SId, Key, _, _, _}, Src={_,_}, Timeout) ->
+controller_loop(Session={SId, Key, _, _, _}, Src={_,_}, Timeouts={SpeakTimeout, Timeout}) ->
     receive
         % client packet
         {packet, P} ->
+            SpeakTimeout ! packet,
             Timeout ! packet,
             case dec_handler(Session, P) of
                 % this may be useful but dialyzer is not happy:
@@ -80,18 +87,20 @@ controller_loop(Session={SId, Key, _, _, _}, Src={_,_}, Timeout) ->
                 %     Encrypted = enc_chunk(Data, Key),
                 %     tasty_listener ! {send, Src, Encrypted};
                 drop ->
+                    logging:log("Dropping ~p", [SId]),
                     tasty:unregister_user(SId),
                     exit(normal);
                 _ -> ok
             end;
         % voice/video data from other client
         {broadcast, P} ->
-            tasty_listener ! {send, Src, enc_chunk(P, Key)}
-    after ?TIMEOUT ->
-        tasty:unregister_user(SId),
-        exit(normal)
+            tasty_listener ! {send, Src, enc_chunk(P, Key)};
+        timeout ->
+            logging:log("Dropping ~p (timed out)", [SId]),
+            tasty:unregister_user(SId),
+            exit(normal)
     end,
-    controller_loop(Session, Src, Timeout).
+    controller_loop(Session, Src, Timeouts).
 
 speaking_status_timeout(Session) ->
     receive
@@ -102,3 +111,11 @@ speaking_status_timeout(Session) ->
         tasty:rm_speaking_flag(Session)
     end,
     speaking_status_timeout(Session).
+
+session_timeout(Parent) ->
+    receive
+        packet -> ok
+    after ?TIMEOUT ->
+        Parent ! timeout
+    end,
+    session_timeout(Parent).
